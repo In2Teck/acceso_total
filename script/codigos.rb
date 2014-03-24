6e2f249b44bba5cad6806ef6e9a2294cf89ac034
AlbertoSimonin <alberto.simonin@in2teck.com>
2014-03-24 13:57:56 -0600

    Corregido script de codigos

--- script/codigos.rb
+++ script/codigos.rb
@@ -1,12 +1,10 @@
 #!/usr/bin/env ruby
 require_relative "../config/environment"
 entrada = File.open(ARGV[0])
-id = 0
 
 entrada.each_line{|codigo|
 	botella = Bottle.new
-	botella.id = id+1
-	botella.code = codigo
+	botella.code = codigo[0, 8]
 	botella.created_at=Date.today
 	botella.updated_at=Date.today
 	botella.save
