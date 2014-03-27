27ff1107beceb5d02081ce0cd70962c63ef8783e
Benjamín Hernández <benjamin.hernandez@in2teck.com>
2014-03-27 13:12:13 -0600

    Validación de botella aún estando participando.

--- app/models/bottle.rb
+++ app/models/bottle.rb
@@ -4,7 +4,15 @@ class Bottle < ActiveRecord::Base
   has_one :participation
 
   def self.validate_code code
-    return Bottle.find_by_code(code)
+    if b = Bottle.find_by_code(code)
+      if Participation.find_by_bottle_id(b.id)
+        return false
+      else
+        return true
+      end
+    else
+      return false
+    end
   end
 
 end
