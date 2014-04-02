0139340e2c925f0f7247856f554f8f1e8cc1c006
Benjamín Hernández <benjamin.hernandez@in2teck.com>
2014-04-02 14:16:53 -0600

    Se creó la leyenda responsive de busqueda de codigo.

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,3 +8,7 @@
 
 role_admin = Role.create(name: 'admin')
 User.create(first_name: 'admin', last_name: 'admin', email: 'admin@admin.com', password: 'adminadmin', roles: [role_admin])
+
+Question.create({"name" => "QUE ES LO QUE METERIAS EN TU MALETA PARA IR A CANNES CON ACCESO TOTAL Y POR QUE?"})
+Question.create({"name" => "AL LLEGAR A CANNES CON ACCESO TOTAL, QUE SERIA LO PRIMERO QUE HARIAS Y POR QUE?"})
+Question.create({"name" => "POR QUE SERIAS LA PERSONA INDICADA PARA IR A UN VIAJE A CANNES, FRANCIA CON ACCESO TOTAL Y TODO PAGADO?"})
